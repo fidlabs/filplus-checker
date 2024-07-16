2a40127c35697ebeb8374532e5458d9978ee5d10
gstraczek <grzegorz.straczek@neti-soft.com>
2024-07-16 09:24:09 +0200

    Add generated reports path to the db

---
+++ migrations/1720788930023_add-generated-reports-table.js
@@ -0,0 +1,34 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.createTable("generated_reports", {
+    id: "id",
+    client_address_id: {
+      type: "varchar",
+      notNull: true,
+    },
+    file_path: { type: "varchar", notNull: true },
+    created_at: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("generated_reports");
+};
